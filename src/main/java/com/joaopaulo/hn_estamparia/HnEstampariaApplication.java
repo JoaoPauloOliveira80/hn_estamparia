19de5e848e3eb449c266bea4dc5fbea4aa02b28d
Dundaki <vigjoaopaulo@gmail.com>
2023-06-25 18:57:03 -0300

    inicio de projeto

---
+++ src/main/java/com/joaopaulo/hn_estamparia/HnEstampariaApplication.java
@@ -0,0 +1,13 @@
+package com.joaopaulo.hn_estamparia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HnEstampariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HnEstampariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaopaulo/hn_estamparia/HnEstampariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaopaulo.hn_estamparia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HnEstampariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
